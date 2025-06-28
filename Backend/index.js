b1dbfa943ea8ecd2c3ec0dac6811d91b9d53f8a0
Piyush Thapa <yesiampiyushthapa@gmail.com>
2025-06-28 12:41:51 +0530

    local mongo server

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URL, {
+    mongoose.connect("mongodb://localhost:27017", {
         dbName: "ProfileFormBackend",
     }).then((c) => console.log(`Database connected with ${c.connection.host}`)).catch((e) => console.log(e))
 }
